ec4c9d7a8d32c551a945393ae0a713af6547eff6
aruniankur <arunitowardnew@gmail.com>
2024-05-18 03:11:12 +0530

    black

--- run.py
+++ run.py
@@ -1,14 +1,14 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from .route import register_routes 
-from .models import db
+from route import register_routes 
+from models import db
 import os
 
 app = Flask(__name__, template_folder='templates')
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/dummy'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/intaligen_db'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://intaligen:RlTBS2h3UfRDYtcoqRPJgklcwvXm3yuM@dpg-cp2fgm8l6cac73de0ra0-a.oregon-postgres.render.com/intaligen_dummy'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/intaligen_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://intaligen:RlTBS2h3UfRDYtcoqRPJgklcwvXm3yuM@dpg-cp2fgm8l6cac73de0ra0-a.oregon-postgres.render.com/intaligen_dummy'
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db.init_app(app)
 register_routes(app, db)
