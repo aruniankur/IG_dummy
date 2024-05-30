4df50f5ef7c64671b02fd90c73c843f2a660713f
aruniankur <arunitowardnew@gmail.com>
2024-05-30 10:03:53 +0530

    created config file

--- run.py
+++ run.py
@@ -1,33 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from route import register_routes 
 from models import db
-from celery import Celery, Task
-from flask import Flask
-
-app = Flask(__name__, template_folder='templates')
-def celery_init_app(app: Flask) -> Celery:
-    class FlaskTask(Task):
-        def __call__(self, *args: object, **kwargs: object) -> object:
-            with app.app_context():
-                return self.run(*args, **kwargs)
-
-    celery_app = Celery(app.name, task_cls=FlaskTask)
-    celery_app.config_from_object(app.config["CELERY"])
-    celery_app.set_default()
-    app.extensions["celery"] = celery_app
-    return celery_app
-
-app.config.from_mapping(
-        CELERY=dict(
-            broker_url="rediss://red-cpaknacf7o1s73ai68og:JZshvN5SXUfCQL4D5i3AJIBM0RSKhWxy@singapore-redis.render.com:6379/0?ssl_cert_reqs=CERT_NONE",
-            result_backend="rediss://red-cpaknacf7o1s73ai68og:JZshvN5SXUfCQL4D5i3AJIBM0RSKhWxy@singapore-redis.render.com:6379/0?ssl_cert_reqs=CERT_NONE",
-            task_ignore_result=True,
-            broker_connection_retry_on_startup=True,
-        ),
-    )
-app.config.from_prefixed_env()
-celery_init_app(app)
+from config import app
 #this is the local data
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/intaligendb2'
